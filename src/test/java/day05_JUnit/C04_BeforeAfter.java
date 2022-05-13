5d87b399287095f5fe107d00094bf90f51121162
zeynepsakrak <100724081+zeynepsakrak@users.noreply.github.com>
2022-05-13 23:44:06 +0300

    Initial commit

---
+++ src/test/java/day05_JUnit/C04_BeforeAfter.java
@@ -0,0 +1,44 @@
+package day05_JUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_BeforeAfter {
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+    @Test
+    public void method1(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.com");
+        driver.close();
+    }
+    @Test
+    public void method2(){
+        driver.get("https://www.techproeducation.com");
+
+    }
+    @Test
+    public void method3(){
+        driver.get("https://www.facebook.com");
+    }
+}
